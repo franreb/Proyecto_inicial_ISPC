d992361126480c5d885f90a956e2ba17a7acff87
cartup90 <104448524+cartup90@users.noreply.github.com>
2022-08-12 11:31:58 -0300

    Script de la database para Trabajo integrador
    Algunas aclaraciones:
    *Lo dejo en la rama Main, pero ustedes deben subirlo a SU RAMA cuando tengan el ejercicio completo
    *El ejercicio 1 y 2 ya está dentro del código, sientanse libres de modificar lo que les parezca
    *El tipo de dato 'Date' (En la columna de Fecha_nac) se inserta de la siguiente manera 'aaaa-mm-dd'   (Entre comillas simples y preferentemente separado por guiones medios)

---
+++ Veterinaria.sql
@@ -0,0 +1,53 @@
+#Se Crea la Base de datos
+CREATE DATABASE Veterinaria;
+
+#Nos posicionamos en la base de datos recién creada
+USE Veterinaria;
+
+
+#Se crea la tabla Dueno con sus restricciones, y hacemos de la columna DNI nuestra PK
+CREATE TABLE Dueno (
+	DNI int not null,
+    NOMBRE varchar(100) not null,
+    APELLIDO varchar(100) not null,
+    TELEFONO int default null,
+    DIRECCION varchar(300),
+    PRIMARY KEY (DNI)
+);
+
+
+#Creamos la tabla Perro, donde ID_Perro es la PK y DNI_DUENO es FK haciendo...
+#...referencia a la columna DNI de la tabla Dueno   
+CREATE TABLE Perro (
+	ID_Perro int not null auto_increment,
+    NOMBRE varchar(50) not null,
+    FECHA_NAC date not null,
+    SEXO varchar(50) not null,
+    DNI_DUENO int not null,
+    PRIMARY KEY (ID_Perro),
+    FOREIGN KEY (DNI_DUENO) REFERENCES DUENO(DNI)
+);
+
+
+#Se crea la Tabla Historial, donde ID_Historial es un valor autoincremental...
+#...Que será nuestra PK, y la columna Perro es FK de ID_Perro de la tabla Perro
+CREATE TABLE Historial (
+	ID_Historial int not null auto_increment,
+    Fecha varchar(50) not null,
+    Perro int not null,
+    Descripción varchar(200),
+    Monto float not null,
+    Primary key (ID_Historial),
+    Foreign key (Perro) REFERENCES Perro(ID_Perro)
+    );
+    
+    
+#Entrada de Datos
+INSERT INTO Dueno (DNI,NOMBRE,APELLIDO,TELEFONO,DIRECCION) Values (34884453, 'CATRIEL','PARDO',154180337,'BUENA VISTA 1192');
+
+INSERT INTO Dueno (DNI,NOMBRE,APELLIDO,TELEFONO,DIRECCION) Values (12345678, 'ROBERTO', 'GOMEZ BOLAÑOS', 154222112, 'BARRILl 5');
+
+INSERT INTO Perro (NOMBRE, FECHA_NAC,SEXO,DNI_DUENO) Values ('KHALI', '2020-06-23', 'HEMBRA', 34884453);
+
+INSERT INTO Perro (NOMBRE, FECHA_NAC,SEXO,DNI_DUENO) Values ('TOBY', '2015-08-17', 'MACHO', 12345678)
+    
